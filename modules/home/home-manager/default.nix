c6cd9042d13456222efa0f956879a6c0ddf8d32c
SphericalKat <me@kat.bio>
2025-09-03 14:49:37 +0900

    feat(modules): add home-manager shell envs and aliases
    Signed-off-by: SphericalKat <me@kat.bio>

--- modules/home/home-manager/default.nix
+++ modules/home/home-manager/default.nix
@@ -2,4 +2,13 @@
   programs.home-manager = {
     enable = true;
   };
+
+  home.sessionVariables = {
+    EDITOR = "vi";
+    HOTEL = "trivago";
+  };
+
+  home.shellAliases = {
+    nvm = "fnm";
+  };
 }
