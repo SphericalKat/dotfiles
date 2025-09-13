0abfafd4a30ed9601708367cfa2ae709208884a0
SphericalKat <me@kat.bio>
2025-09-14 01:43:58 +0900

    feat(darwin): add macbook air
    Signed-off-by: SphericalKat <me@kat.bio>

---
+++ homes/aarch64-darwin/sphericalkat@Amoghs-MacBook-Air/default.nix
@@ -0,0 +1,8 @@
+_: {
+  snowfallorg.user = {
+    enable = true;
+    name = "sphericalkat";
+  };
+
+  home.stateVersion = "24.05";
+}
---
+++ systems/aarch64-darwin/Amoghs-MacBook-Air/default.nix
@@ -0,0 +1,73 @@
+{ pkgs, namespace, lib, inputs, ... }:
+{
+  # Nix configuration
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-users = [ "root" "sphericalkat" ];
+  };
+
+  # System configuration
+  system.stateVersion = 4;
+  system.primaryUser = "sphericalkat";
+
+  # Fix nixbld group GID mismatch
+  ids.gids.nixbld = 350;
+
+  users.users.sphericalkat = {
+    name = "sphericalkat";
+    home = "/Users/sphericalkat";
+  };
+
+  environment.variables = {
+    LANG = "en_US.UTF-8";
+  };
+
+  environment.systemPackages = with pkgs; [
+    nh
+    zoxide
+    ansible
+    parallel
+    git
+    gh
+    just
+    ctags
+    ripgrep
+    fd
+    bat
+    cocoapods
+    fnm
+    direnv
+    deno
+    flyctl
+  ];
+
+  # Home Manager configuration
+  home-manager = {
+    backupFileExtension = "bak";
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+
+  # Enable homebrew
+  homebrew = {
+    enable = true;
+    brews = [
+      "pgcli"
+    ];
+    casks = [
+      "alt-tab"
+      "obsidian"
+      "orbstack"
+      "prismlauncher"
+      "raycast"
+      "rectangle"
+      "spotify"
+      "stolendata-mpv"
+      "zed"
+      "ghostty"
+      "claude-code"
+      "figma"
+    ];
+    taps = [ ];
+  };
+}
